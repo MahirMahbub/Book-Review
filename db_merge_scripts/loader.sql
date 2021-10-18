2b46ef22ff6f32879a8c450ec38c2ceb2875542b
MahirMahbub <bsse0807@iit.du.ac.bd>
2021-10-18 14:33:13 +0600

    modify sql loader

--- db_merge_scripts/loader.sql
+++ db_merge_scripts/loader.sql
@@ -1,16 +1,16 @@
-TRUNCATE TABLE public.book;
+-- TRUNCATE TABLE public.book;
 
 INSERT INTO public.book(id, name, author_name, isbn)
 VALUES (1, 'NoWhere','MM', '4453252532'),
        (2, 'Anywhere','NoWhere', '5352525535');
 
-TRUNCATE TABLE public.user;
+-- TRUNCATE TABLE public.user;
 
 INSERT INTO public.user(id, first_name, last_name)
 VALUES (1, 'A', 'User B'),
        (2, 'C', 'User D');
-
-TRUNCATE TABLE public.user_review;
+--
+-- TRUNCATE TABLE public.user_review;
 INSERT INTO public.user_review(id, review, rating, user_id, book_id)
 VALUES (1, 'A Good Book', 5, 1, 1),
        (2, 'A Great Book', 5, 1, 1),
